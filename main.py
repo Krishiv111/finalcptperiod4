957610bbde449994b27a7d82b3e25486ac903df6
T2fastpage <12james15545@gmail.com>
2023-02-10 13:33:38 -0800

    flask mistake fixes

--- main.py
+++ main.py
@@ -44,6 +44,5 @@ def activate_job():
 if __name__ == "__main__":
     # change name for testing
     #from flask_cors import CORS
-    #cors = CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
+    #cors = CORS(app)   
     app.run(debug=True, host="0.0.0.0", port="8086")
