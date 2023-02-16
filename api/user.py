238f71da3836a6a74dc845a058d292668c7bc6bf
Krishiv111 <krishivmahendru2007@gmail.com>
2023-02-16 14:00:49 -0800

    pppppp

--- api/user.py
+++ api/user.py
@@ -1,3 +1,4 @@
+import json
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
@@ -20,14 +21,23 @@ class UserAPI:
             # validate name
             name = body.get('name')
             if name is None or len(name) < 2:
-                return {'message': f'Name is missing, or is less than 2 characters'}, 210
+                return {'message': f'Name is missing, or is less than 2 characters'}, 400
             # validate uid
             uid = body.get('uid')
             if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 210
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
             # look for password and dob
-            password = body.get('password')
-            dob = body.get('dob')
+
+            baths = body.get('baths')
+            if baths is None or len(baths) < 0:
+                return {'message': f'Baths is missing, or is less than 2 characters'}, 210
+            beds = body.get('beds')
+            if beds is None or len(beds) < 1:
+                return {'message': f'Beds is missing, or is less than 2 characters'}, 210
+            price = body.get('price')
+            if price is None or len(price) < 1:
+                return {'message': f'Price is missing, or is less than 2 characters'}, 210
+            
 
             ''' #1: Key code block, setup USER OBJECT '''
             uo = User(name=name, 
@@ -35,14 +45,7 @@ class UserAPI:
             
             ''' Additional garbage error checking '''
             # set password if provided
-            if password is not None:
-                uo.set_password(password)
-            # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
+            
             
             ''' #2: Key Code block to add user to database '''
             # create user in database
@@ -51,14 +54,36 @@ class UserAPI:
             if user:
                 return jsonify(user.read())
             # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 210
+            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
 
     class _Read(Resource):
         def get(self):
             users = User.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+    
+    class _Security(Resource):
+
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            ''' Get Data '''
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
+            
+            
+            ''' Find user '''
+            
+            
+            ''' authenticated user '''
+            
+
+            
 
     # building RESTapi endpoint
     api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
+    api.add_resource(_Security, '/authenticate')
+    
