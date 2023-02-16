5bbcc35e7117496893065896ccbec2b57f54b6a4
T2fastpage <12james15545@gmail.com>
2023-02-15 23:24:28 -0800

    test

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
-from model.activity import initActivity
 
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
-from api.activity import activity_api
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -23,7 +21,6 @@ from projects.projects import app_projects # Blueprint directory import projects
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
-app.register_blueprint(activity_api)
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -43,7 +40,6 @@ def stub():
 def activate_job():
     initJokes()
     initUsers()
-    initActivity()
 
 # this runs the application on the development server
 if __name__ == "__main__":
