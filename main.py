f4e1afca02ff35433a2b16133a5dd50028da8bd6
Krishiv111 <krishivmahendru2007@gmail.com>
2023-02-21 15:02:50 -0800

    krishiv feature push with remap

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from flask import render_template  # import render_template from "public" flask 
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
 from model.jokes import initJokes
-from model.users import initUsers
+from model.houses import initHouses
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+from api.house import house_api # Blueprint import api definition
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -19,7 +19,7 @@ from projects.projects import app_projects # Blueprint directory import projects
 # register URIs
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
+app.register_blueprint(house_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -38,7 +38,7 @@ def stub():
 @app.before_first_request
 def activate_job():
     initJokes()
-    initUsers()
+    initHouses()
 
 # this runs the application on the development server
 if __name__ == "__main__":
